ade556bf7454b23dfeb943717cb7e783d2e0f5b3
bartoszjas30 <bartoszjas20@gmail.com>
2022-03-27 19:17:18 +0200

    remove active articles

--- js/script.js
+++ js/script.js
@@ -4,9 +4,18 @@ function titleClickHandler(event){
     console.log(event);
 
   /* remove class 'active' from all article links  */
+  const activeLinks = document.querySelectorAll('.title a.active');
+
+  for(let activeLink of activeLinks){
+      activeLink.classList.remove('active');
+  }
 
   /* add class 'active' to the clicked link */
+  const activeArticles = document.querySelectorAll('.posts article.active');
 
+  for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
+  }
   /* remove class 'active' from all articles */
 
   /* get 'href' attribute from the clicked link */
